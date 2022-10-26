a23f261a21b0fd7fc4bcd1cdef3105f72a7b687c
Bohdan <makohoniuk.bo@gmail.com>
2022-10-26 23:08:30 +0300

    addMarkup

--- src/components/FeedbackFromPeople/feedback.jsx
+++ src/components/FeedbackFromPeople/feedback.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 
 
-class FeedBackPeople extends React.Component{
+class FeedBackPeople extends React.Component {
 
-state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
   
-}
+    }
     
     buttnonGood = () => {
         this.setState(prevState => {
@@ -35,42 +35,42 @@ state = {
 
     countTotalFeedbackPeople = () => {
         const { good, neutral, bad } = this.state;
-      if (!good) {
-         return 0;
-      }
-      const total = good + neutral + bad;
-      const feedback = (good * 100) / total;
-      return Math.round(feedback);
-   };
-    }
+        if (!good) {
+            return 0;
+        }
+        const total = good + neutral + bad;
+        const feedback = (good * 100) / total;
+        return Math.round(feedback);
+    };
+    
        
     
-    // countTotalFeedbackPeople = ({ good, neutral, bad }) => {
-    //     return (
-    //         <div>
-    //                 <p>Statistics</p>
-    //                 <p>Good:{good}</p>
-    //                 <p>Neutral:{neutral}</p>
-    //                 <p>Bed:{bad}</p>
-    //                 <p>Total:{()=>{}}</p>
-    //         </div>)
+    countTotalFeedbackPeople = ({ good, neutral, bad }) => {
+        return (
+            <div>
+                <p>Statistics</p>
+                <p>Good:{good}</p>
+                <p>Neutral:{neutral}</p>
+                <p>Bed:{bad}</p>
+                <p>Total:{() => { }}</p>
+            </div>)
  
-    // };
+    };
     
 
-render() {
-    return (
-        <div>
+    render() {
+        return (
+            <div>
                 <p>Please leave feedback</p>
 
                 <button type='button' onClick={this.buttnonGood}>Good</button>
                 <button type='button' onClick={this.buttnonNeutral}>Neutral</button>
-                <button type='button' onClick={this.buttnonBad}>Bed</button>  
-        </div>
-            );
+                <button type='button' onClick={this.buttnonBad}>Bed</button>
+            </div>
+        );
         
-};
-
+    };
+}
 
 
 export default FeedBackPeople;
