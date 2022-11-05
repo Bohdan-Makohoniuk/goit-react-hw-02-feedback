8b23691f042601b02bb98e1c7c99bd9c0c6c4ac5
Bohdan <makohoniuk.bo@gmail.com>
2022-11-05 15:18:20 +0200

    add_min_styled

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -3,11 +3,18 @@ import styled from '@emotion/styled'
 export const Btn = styled.button`
     margin-left:10px;
     border-radius:3px;
-    border:none;    
+    border:none;   
+    background-color:#c7cab8; 
+    &:hover,
+    &:focus
+    {background-color:white;
+        color: #449aab};
     `
 export const containerBtn =styled.div`
     display:flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    border-radius: 4px;
+    box-sizing: border-box;
 `       
